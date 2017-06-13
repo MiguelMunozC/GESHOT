0b44b2f14f37195c9b29f754b80de63dff501522
FernandoRiffo <fdo.riffo93@gmail.com>
2017-06-12 20:49:37 -0400

    modificaciones clase 12-06

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-session_start(); 
+session_start();
 include "php/conexion.php";
 ?>
 <html>
@@ -28,20 +28,20 @@ include "php/conexion.php";
     </head>
     <body class="font-cover" id="login">
         <div class="container-login center-align">
-            <div style="margin:15px 0;">
+            <div id="titulo" style="margin:15px 0;">
                 <i class="zmdi zmdi-account-circle zmdi-hc-5x"></i>
                 <p>Inicia sesión con tu cuenta</p>   
             </div>
-            <form action="php/validarUser.php" method="post">
+            <form id="formularioIngreso" method="post">
                 <div class="input-field">
-                    <input name="username" type="text" class="validate">
-                    <label for="username"><i class="zmdi zmdi-account"></i> Nombre</label>
+                    <input id="nameInput" name="username" type="text" class="validate">
+                    <label id="nameLabel" for="username"><i class="zmdi zmdi-account"></i> Nombre</label>
                 </div>
                 <div class="input-field col s12">
-                    <input name="password" type="password" class="validate">
-                    <label for="password"><i class="zmdi zmdi-lock"></i> Contraseña</label>
+                    <input id="passwordInput" name="password" type="password" class="validate">
+                    <label id="passwordLabel" for="password"><i class="zmdi zmdi-lock"></i> Contraseña</label>
                 </div>
-                <button type="submit" value="acceder" class="waves-effect waves-teal btn-flat">Ingresar <i class="zmdi zmdi-mail-send"></i></button>
+                <button id="acceder" type="button" value="acceder" class="waves-effect waves-teal btn-flat">Ingresar <i class="zmdi zmdi-mail-send"></i></button>
             </form>
         </div>
 
@@ -60,6 +60,40 @@ include "php/conexion.php";
 
         <!-- MaterialDark JS -->
         <script src="js/main.js"></script>
+        <script type="text/javascript">
+            $(document).ready(function () {
+                $("#acceder").click(function () {
+                    $.ajax({
+                        type: "POST",
+                        url: "php/validarUser.php",
+                        data: $("#formularioIngreso").serialize(), // post data || get data
+                        success: function (result) {
+                            if (result == -1) {
+                                $("#titulo").css("color", "red");
+                                $("#nameLabel").css("color", "red");
+                                $("#passwordLabel").css("color", "red");
+                            } else {
+                                window.location.replace(result);
+                            }
+                        },
+                        error: function (xhr, resp, text) {
+                            window.location.replace(result);
+                        }
+                    });
+                });
+            });
+
+            $("#nameInput").focus(function () {
+                $("#titulo").css("color", "black");
+                $("#nameLabel").css("color", "black");
+                $("#passwordLabel").css("color", "black");
+            });
+            $("#passwordInput").focus(function () {
+                $("#titulo").css("color", "black");
+                $("#nameLabel").css("color", "black");
+                $("#passwordLabel").css("color", "black");
+            });
+        </script>
     </body>
 </html>
 

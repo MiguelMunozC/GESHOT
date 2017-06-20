2f1fa2e9bb4fe642f23b94e69aa9bf79d6b58f36
FernandoRiffo <fdo.riffo93@gmail.com>
2017-06-20 08:30:22 -0400

    change home
    se agrega el cierre del tag de body

--- home.php
+++ home.php
@@ -26,79 +26,81 @@ include 'conexion.php';
         <!-- MaterialDark CSS -->
         <link rel="stylesheet" href="css/style.css">
     </head>
-
-    <!-- Nav Lateral -->
-    <section class="NavLateral full-width">
-        <div class="NavLateral-FontMenu full-width ShowHideMenu"></div>
-        <div class="NavLateral-content full-width">
-            <header class="NavLateral-title full-width center-align">
-                GesHot <i class="zmdi zmdi-close NavLateral-title-btn ShowHideMenu"></i>
-            </header>
-            <figure class="full-width NavLateral-logo">
-                <img src="assets/img/hotel.jpg" alt="material-logo" class="responsive-img center-box">
-            </figure> 
-            <div class="NavLateral-Nav">
-                <ul class="full-width">
-                    <li>
-                        <a href="php/formularioHuesped.php" class="waves-effect waves-light"><i class="zmdi zmdi-desktop-mac zmdi-hc-fw"></i> Ingreso Huesped</a>
-                    </li>
-                    <li class="NavLateralDivider"></li>
-                    <li>
-                        <a href="#" class="NavLateral-DropDown  waves-effect waves-light"><i class="zmdi zmdi-language-css3 zmdi-hc-fw"></i> <i class="zmdi zmdi-chevron-down NavLateral-CaretDown"></i> Registro Habitación</a>
-                        <ul class="full-width">
-                            <li><a href="typography-shadow.html" class="waves-effect waves-light">Check In</a></li>
-                            <li class="NavLateralDivider"></li>
-                            <li><a href="table.html" class="waves-effect waves-light">Check Out</a></li>
-                        </ul>
-                    </li>
-                </ul>
+    <body>
+        <!-- Nav Lateral -->
+        <section class="NavLateral full-width">
+            <div class="NavLateral-FontMenu full-width ShowHideMenu"></div>
+            <div class="NavLateral-content full-width">
+                <header class="NavLateral-title full-width center-align">
+                    GesHot <i class="zmdi zmdi-close NavLateral-title-btn ShowHideMenu"></i>
+                </header>
+                <figure class="full-width NavLateral-logo">
+                    <img src="assets/img/hotel.jpg" alt="material-logo" class="responsive-img center-box">
+                </figure> 
+                <div class="NavLateral-Nav">
+                    <ul class="full-width">
+                        <li>
+                            <a href="php/formularioHuesped.php" class="waves-effect waves-light"><i class="zmdi zmdi-desktop-mac zmdi-hc-fw"></i> Ingreso Huesped</a>
+                        </li>
+                        <li class="NavLateralDivider"></li>
+                        <li>
+                            <a href="#" class="NavLateral-DropDown  waves-effect waves-light"><i class="zmdi zmdi-language-css3 zmdi-hc-fw"></i> <i class="zmdi zmdi-chevron-down NavLateral-CaretDown"></i> Registro Habitación</a>
+                            <ul class="full-width">
+                                <li><a href="typography-shadow.html" class="waves-effect waves-light">Check In</a></li>
+                                <li class="NavLateralDivider"></li>
+                                <li><a href="table.html" class="waves-effect waves-light">Check Out</a></li>
+                            </ul>
+                        </li>
+                    </ul>
+                </div>  
             </div>  
-        </div>  
-    </section>
+        </section>
 
-    <!-- Page content -->
-    <section class="ContentPage full-width">
-        <!-- Nav Info -->
-        <div class="ContentPage-Nav full-width">
-            <ul class="full-width">
-                <li><figure><img src="assets/img/user.png" alt="UserImage"></figure></li>
-                <li style="padding:0 5px;">Recepcionista Name</li>
-                <li><a href="#" class="tooltipped waves-effect waves-light btn-ExitSystem" data-position="bottom" data-delay="50" data-tooltip="Logout"><i class="zmdi zmdi-power"></i></a></li>
-            </ul>   
-        </div>
+        <!-- Page content -->
+        <section class="ContentPage full-width">
+            <!-- Nav Info -->
+            <div class="ContentPage-Nav full-width">
+                <ul class="full-width">
+                    <li><figure><img src="assets/img/user.png" alt="UserImage"></figure></li>
+                    <li style="padding:0 5px;">Recepcionista Name</li>
+                    <li><a href="#" class="tooltipped waves-effect waves-light btn-ExitSystem" data-position="bottom" data-delay="50" data-tooltip="Logout"><i class="zmdi zmdi-power"></i></a></li>
+                </ul>   
+            </div>
 
-        <div class="row">
-            <!-- Tiles -->
-            <article class="col s12">
-                <div class="full-width center-align" style="margin: 40px 0;">
-                    <div class="tile">
-                        <div class="tile-icon"><i class="zmdi zmdi-mail-send"></i></div>
-                        <div class="tile-caption">
-                            <span class="center-align">0</span>
-                            <p class="center-align">Huespedes Registrados</p>   
+            <div class="row">
+                <!-- Tiles -->
+                <article class="col s12">
+                    <div class="full-width center-align" style="margin: 40px 0;">
+                        <div class="tile">
+                            <div class="tile-icon"><i class="zmdi zmdi-mail-send"></i></div>
+                            <div class="tile-caption">
+                                <span class="center-align">0</span>
+                                <p class="center-align">Huespedes Registrados</p>   
+                            </div>
+                            <a href="#" class="tile-link waves-effect waves-light">View Details &nbsp; <i class="zmdi zmdi-caret-right-circle"></i></a>
                         </div>
-                        <a href="#" class="tile-link waves-effect waves-light">View Details &nbsp; <i class="zmdi zmdi-caret-right-circle"></i></a>
-                    </div>
-                    <div class="tile">
-                        <div class="tile-icon"><i class="zmdi zmdi-shopping-cart"></i></div>
-                        <div class="tile-caption">
-                            <span class="center-align">0</span>
-                            <p class="center-align">Piezas Disponibles</p>   
+                        <div class="tile">
+                            <div class="tile-icon"><i class="zmdi zmdi-shopping-cart"></i></div>
+                            <div class="tile-caption">
+                                <span class="center-align">0</span>
+                                <p class="center-align">Piezas Disponibles</p>   
+                            </div>
+                            <a href="#" class="tile-link waves-effect waves-light">View Details &nbsp; <i class="zmdi zmdi-caret-right-circle"></i></a>
                         </div>
-                        <a href="#" class="tile-link waves-effect waves-light">View Details &nbsp; <i class="zmdi zmdi-caret-right-circle"></i></a>
-                    </div>
-                    <div class="tile">
-                        <div class="tile-icon"><i class="zmdi zmdi-card"></i></div>
-                        <div class="tile-caption">
-                            <span class="center-align">0</span>
-                            <p class="center-align">Habitaciones Ocupadas</p>   
+                        <div class="tile">
+                            <div class="tile-icon"><i class="zmdi zmdi-card"></i></div>
+                            <div class="tile-caption">
+                                <span class="center-align">0</span>
+                                <p class="center-align">Habitaciones Ocupadas</p>   
+                            </div>
+                            <a href="#" class="tile-link waves-effect waves-light">View Details &nbsp; <i class="zmdi zmdi-caret-right-circle"></i></a>
                         </div>
-                        <a href="#" class="tile-link waves-effect waves-light">View Details &nbsp; <i class="zmdi zmdi-caret-right-circle"></i></a>
-                    </div>
-                </div>   
-            </article>
-        </div>
+                    </div>   
+                </article>
+            </div>
 
+
+        </section>
         <!-- Footer -->   
         <footer class="footer-MaterialDark">
             <div class="NavLateralDivider"></div>
@@ -108,8 +110,7 @@ include 'conexion.php';
                 </div>
             </div>
         </footer>
-    </section>
-
+    </body>
     <!-- Sweet Alert JS -->
     <script src="js/sweetalert.min.js"></script>
 

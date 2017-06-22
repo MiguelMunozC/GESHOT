6cb9401501441aeca934fd62d1e6c65ac4f799aa
Rolejo <rolejo.rock@hotmail.com>
2017-06-22 19:13:48 -0400

    Boton agregar
    agregado boton que agrega y resetea el formulario

--- php/formularioHuesped.php
+++ php/formularioHuesped.php
@@ -16,70 +16,114 @@ include "conexion.php";
     <link href="http://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
     <link href="../css/materialize/css/materialize.css" rel="stylesheet" type="text/css"/>
     <body>
-        <form action="registrarHuesped.php" method="post" class="col s12">
-            <h2>Formulario Huesped</h2>
+        <h2 class="center-align">Formulario Huesped</h2>
 
 
-            <div class="row" >
-                <div class="input-field col s3">
-                    <input value="" name= "nombreHuesped" id="nombreHuesped" type="text" class="validate" required>
-                    <label class="active"  for="nombreHuesped"  >Nombre</label> 
+        <div class="row">
+            <article class="col s6 offset-s3">
+                <form action="registrarHuesped.php" method="post"  id="form">
 
-                </div>
+                    <div class="row" >
+ 
+                        <div class="input-field col s6">
+                             <i class="material-icons prefix">perm_identity</i>
+                            <input  value="" name= "nombreHuesped" id="nombreH" type="text" class="validate"  required>
+                            <label class="active"  for="nombreHuesped" >Nombre</label> 
+                        </div>
 
-                <div class="input-field col s3">
-                    <input value="" name= "apellidoHuesped" id="apellidoHuesped" type="text" class="validate" required>
-                    <label class="active" for="apellidoHuesped">Apellido</label>
-                </div>
-            </div>
-            <div class="row">
-                <div class="input-field col s6">
-                    <input value="" name="nacionalidad" id="nacionalidad" type="text" class="validate" required>
-                    <label class="active" for="nacionalidad">Nacionalidad</label>
-                </div>
-            </div>
-            <div class="row">
-                <div class="input-field col s6">
-                    <input value="" name="fechaNacimiento" id="fechaNacimiento" type="text" class="validate" required>
-                    <label class="active" for="fechaNacimiento">Fecha Nacimiento</label>
-                </div>
-            </div>
-            <div class="row">
-                <div class="input-field col s6">
-                    <input value="" name="dni" id="dni" type="text" class="validate" required>
-                    <label class="active" for="dni">DNI</label>
-                </div>
-            </div>
-            <div class="row">
-                <div class="input-field col s6">
-                    <input value="" name="domicilio" id="domicilio" type="text" class="validate" >
-                    <label class="active" for="domicilio">Domicilio</label>
-                </div>
-            </div>
-            <div class="row">
-                <div class="input-field col s6">
-                    <input value="" name="ocupacion" id="ocupacion" type="text" class="validate" >
-                    <label class="active" for="ocupacion">Ocupacion</label>
-                </div>
-            </div>
-            
-            <div class="row">
-                <div class="input-field col s6">
-                    <input name="email" id="email" type="email" class="validate">
-                    <label class="active" for="email" data-error="wrong" data-success="right">Email</label>
-                </div>
-            </div>
+                        <div class="input-field col s6">
+                            <i class="material-icons prefix">person_pin</i>
+                            <input value="" name= "apellidoHuesped" id="apellidoHuesped" type="text" class="validate" required>
+                            <label class="active" for="apellidoHuesped">Apellido</label>
+                        </div>
+                    </div>
+                    
+                    <div class="row">
+                        <div class="input-field">
+                            <i class="material-icons prefix">portrait</i>
+                            <input value="" name="nacionalidad" id="nacionalidad" type="text" class="validate" required>
+                            <label class="active" for="nacionalidad">Nacionalidad</label>
+                        </div>
+                    </div>
+                    
+                    <div class="row">
+                        <div class="input-field">
+                            <i class="material-icons prefix">date_range</i>
+                            <input value="" name="fechaNacimiento" id="fechaNacimiento" type="text" class="validate" required>
+                            <label class="active" for="fechaNacimiento">Fecha Nacimiento</label>
+                        </div>
+                    </div>
+                    
+                    <div class="row">
+                        <div class="input-field">
+                            <i class="material-icons prefix">account_box</i>
+                            <input value="" name="dni" id="dni" type="text" class="validate" required >
+                            <label  class="active" for="dni">DNI</label>
+                        </div>
+                    </div>
+                    
+                    <div class="row">
+                        <div class="input-field">
+                            <i class="material-icons prefix">home</i>
+                            <input value="" name="domicilio" id="domicilio" type="text" class="validate" >
+                            <label class="active" for="domicilio">Domicilio</label>
+                        </div>
+                    </div>
+                    
+                    <div class="row">
+                        <div class="input-field">
+                            <i class="material-icons prefix">work</i>
+                            <input value="" name="ocupacion" id="ocupacion" type="text" class="validate" >
+                            <label class="active" for="ocupacion">Ocupacion</label>
+                        </div>
+                    </div>
 
-            <button class="btn waves-effect waves-light btn" type="reset" name="action">Reset</button>
-            <button class="btn waves-effect waves-light" type="submit" name="action">Enviar
-                <i class="material-icons right"></i>
-            </button>
+                    <div class="row">
+                        <div class="input-field">
+                            <i class="material-icons prefix">email</i>
+                            <input name="email" id="email" type="email" class="validate" value="" >
+                            <label class="active" for="email" data-error="wrong" data-success="right">Email</label>
+                        </div>
+                    </div>
+
+                    <p class="center-align"> 
+                        <button class="btn waves-effect waves-light btn" type="reset" name="action" id="agregar" >Agregar
+                        <i class="material-icons right">replay</i></button>
+                        <button class="btn waves-effect waves-light" type="submit" name="action" >Agregar y Finalizar
+                            <i class="material-icons right">send</i>
+                        </button>
+                    </p>
 
 
-        </form>
 
-        <?php
-// put your code here
-        ?>
+                </form>
+            </article>
+
+        </div>
+        <script src="../js/jquery-3.2.1.min.js" type="text/javascript"></script>
+        <script type="text/javascript">
+
+            $(function () {
+
+                $("#agregar").click(function () {
+                    var url = "registrarHuesped.php";
+
+                    $.ajax({
+                        type: "POST",
+                        url: url,
+                        data: $("form").serialize(),
+                        success: function () {
+                            alert("Agregado Exitosamente");
+                        }
+                    });
+
+                });
+            });
+
+        </script>
+
+
+
+
     </body>
 </html>
--- php/registrarHuesped.php
+++ php/registrarHuesped.php
@@ -5,8 +5,6 @@ if (!empty($_POST)) {
     if (isset($_POST["nombreHuesped"]) && isset($_POST["apellidoHuesped"]) && isset($_POST["nacionalidad"]) && isset($_POST["fechaNacimiento"]) && isset($_POST["dni"]) && isset($_POST["domicilio"]) && isset($_POST["ocupacion"]) && isset($_POST["email"])) {
         if ($_POST["nombreHuesped"] != "" && $_POST["apellidoHuesped"] != "" && $_POST["nacionalidad"] != "" && $_POST["fechaNacimiento"] != "" && $_POST["dni"] != "" && $_POST["email"] != "") {
 
-            echo "hola";
-            $sql = "insert into huespedes(nombre_huesped,apellido_huesped,DNI,nacionalidad,domicilio,ocupacion,fecha_nacimiento,email_huesped) value (\"$_POST[nombreHuesped]\",\"$_POST[apellidoHuesped]\",\"$_POST[dni]\",\"$_POST[nacionalidad]\",\"$_POST[domicilio]\",\"$_POST[ocupacion]\",\"$_POST[fechaNacimiento]\",\"$_POST[email]\")";
 
             $query = $con->query($sql);
             echo "sql " . $sql;

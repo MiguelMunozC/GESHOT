458cd8e8604c93f6a08734ba16ff104b42aa1e52
Gustavo <thabooox@gmail.com>
2017-07-13 19:47:17 -0400

    curd

---
+++ php/crud.php
@@ -0,0 +1,41 @@
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Modificador</title>
+</head>
+<link href="../css/materialize/css/materialize.css" rel="stylesheet" type="text/css"/>
+ <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+<?php
+include 'conexion.php';
+?>
+
+
+ <div class="row">
+            <article class="col s6 offset-s3">
+
+ <table class="bordered">  
+
+  <div>
+    <h5 class="left-align">Modificador</h5>
+  </div>          
+                <tr>
+                    <th>Nombre</th>
+                    <th>Apellido</th>
+                    <th>DNI</th>
+                    <th>Nacionalidad</th>
+                    <th>domicilio</th>
+                    <th>Ocupacion</th>
+                    <th>Fecha de Nacimiento</th>
+                    <th>E-Mail</th>
+                    <th>ID habitacion<th>
+                    <th> <a class="btn btn-floating btn-large cyan pulse"><i class="material-icons">edit</i></a> <th>
+                </tr> 
+ </table>
+-- configuro el espacio de la pagina  
+
+ </div
+<body>
+
+
+</body>
+</html>

dc4a9850dca3cf37b823a4930c37454e05cda2e7
root <root@MickPC>
2017-07-13 19:32:47 -0400

    DB sin contraseña

--- php/conexion.php
+++ php/conexion.php
@@ -2,7 +2,7 @@
 
 $host = "localhost";
 $user = "root";
-$password = "1234";
+$password = "";
 $db = "geshot";
 $con = mysqli_connect($host, $user, $password, $db) or die('No se puedo conectar a la base de datos'. mysqli_error($con));
 ?>

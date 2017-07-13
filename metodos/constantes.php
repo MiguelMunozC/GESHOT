428670c813b671c5a99174c588c174480541df9f
Rolejo <rolejo.rock@hotmail.com>
2017-07-13 17:24:33 -0400

    eee funca?
    e.e constante

--- metodos/constantes.php
+++ metodos/constantes.php
@@ -1,4 +1,4 @@
 <?php
 define("QUERY_ESTADO_HABITACION", "SELECT ID_habitacion, numero_pieza FROM habitaciones WHERE ID_estado_habitacion =?");
-
+define("QUERY_INSERT_HUESPED", "INSERT INTO huespedes (nombre_huesped,apellido_huesped,DNI,nacionalidad,domicilio,ocupacion,fecha_nacimiento,email_huesped) VALUES (?,?,?,?,?,?,?,?)");
 ?>

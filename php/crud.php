462914f28751c086d569506b222d205829b73c01
Gustavo <thabooox@gmail.com>
2017-07-14 16:48:21 -0400

    crud ordenado

--- php/crud.php
+++ php/crud.php
@@ -4,34 +4,27 @@
 include 'conexion.php';
 ?>
 
-
-
 <div class = "container">
 
- <div class="row">
-         
+ 	<div class="row">
+ 		<table class= "striped" style="width:100%"	>  
 
- <table class= "striped" style="width:100%"	>  
+  			<div>
+   				<h5 class="left-align">Modificador</h5>
+  			</div> 
 
-  <div>
-
-	
-    <h5 class="left-align">Modificador</h5>
-  </div>          
-                <tr>
-                    <th>Nombre</th>
-                    <th>Apellido</th>
-                    <th>DNI</th>
-                    <th>Nacionalidad</th>
-                    <th>domicilio</th>
-                    <th>Ocupacion</th>
-                    <th>Fecha de Nacimiento</th>
-                    <th>E-Mail</th>
-                    <th>ID habitacion<th>
-                    <th><a class="btn btn-floating btn-large cyan pulse"><i class="material-icons">edit</i></a><th>
-                </tr> 
- </table>
--- configuro el espacio de la pagina  
-
- </div>
+            <tr>
+                <th>Nombre</th>
+                <th>Apellido</th>
+                <th>DNI</th>
+                <th>Nacionalidad</th>
+                <th>domicilio</th>
+                <th>Ocupacion</th>
+                <th>Fecha de Nacimiento</th>
+                <th>E-Mail</th>
+                <th>ID habitacion<th>
+                <th><a class="btn btn-floating btn-large cyan pulse"><i class="material-icons">edit</i></a><th>
+            </tr> 
+ 		</table>
+ 	</div>
 </div>
